402f28874d4078b402ef0e7d198b143790294f4f
thedocbwarren <jedi.master@augmentedjs.com>
2019-05-22 20:34:01 -0700

    adding a field

--- src/views/createModelView.js
+++ src/views/createModelView.js
@@ -12,12 +12,17 @@ class CreateModelView extends DirectiveView {
 
     this.template = `
       <h1>Create Model</h1>
-
+      <form>
+        <label for="name">Name
+        <input data-${this.name}="name" type="text" name="name" id="name"/>
+        </label>
+      </form>
     `;
   };
 
   async render() {
     await super.render();
+    this.syncBoundElement("name");
     //this.delegateEvents();
     return this;
   };
