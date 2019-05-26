cb7aec5c0de17b08f21baaf31a171af7ff4b2ac3
thedocbwarren <jedi.master@augmentedjs.com>
2019-05-25 22:17:39 -0700

    updates

--- src/views/createModelView.js
+++ src/views/createModelView.js
@@ -32,31 +32,50 @@ class CreateModelView extends DirectiveView {
   };
 
   async add(e) {
+    console.log("Adding...");
     e.preventDefault();
     const list = Dom.selector(`#${PROPERTY_LIST}`);
     if (list) {
       const li = document.createElement("li");
-      li.innerText = "[Type] Name [Contriants]";
+      li.innerHTML = `
+        <label>Type
+          <select>
+            <option value="string">String</option>
+            <option value="number">Number</option>
+            <option value="array">Array</option>
+          </select>
+        </label>
+        <label>Name
+          <input type="text" name="name" placeholder="Name"/>
+        </label>
+        <label>Min
+          <input type="number" name="min" min="0" class="hidden"/>
+        </label>
+        <label>Max
+          <input type="number" name="max" min="0" class="hidden"/>
+        </label>
+        <label>Regex
+          <input type="type" name="regex"/>
+        </label>
+      `;
       const button = document.createElement("button");
-      button.dataset[this.name] = "remove";
+      button.dataset[this.name] = "removeprop";
       button.dataset.click = "removeprop";
       button.innerHTML = `<i class="material-icons md-dark">clear</i>`;
       li.append(button);
       await list.append(li);
-
     }
     return this;
   };
 
   async removeprop(e) {
-    e.preventDefault();
+    await e.preventDefault();
     const list = Dom.selector(`#${PROPERTY_LIST}`);
     const item = e.target.parentNode;
     if (list && item) {
       await list.removeChild(item);
-      this.delegateEvents();
     }
-    return this;
+    return false;
   };
 
   async render() {
